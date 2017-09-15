0729248ee2c0e1c51edec421268b67646c0e0a9e
Naser Alavi <alavinas@msu.edu>
2017-09-15 00:30:12 -0400

    Removing the add button from the search page and fixing empty array issues

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false,
+    query: '',
     books: []
   }
 
@@ -51,11 +52,33 @@ class BooksApp extends React.Component {
     })
 
   }
+  updateQuery = (query) => {
+     this.setState({ query: query.trim() })
+     console.log('in updateQuery function! query = ' + query)
+     query.length !=0 && BooksAPI.search(query,20).then((books) => {
+         this.setState({ books })
+     })
+   }
 
+  searchBooks = (query) => {
+    console.log('in searchBooks function! query = ' + query)
+if (query) {
+    BooksAPI.search(query,20).then((books) => {
+        this.setState({ books })
+    })
+  }
+}
+
+  clearQuery = () => {
+      this.setState({ query: '' })
+    }
 
 
 
   render() {
+    //const { query } = this.state.query
+
+
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -71,25 +94,78 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" placeholder="Search by title or author"
+                  value={this.state.query}
+                  onChange={(event) =>  this.updateQuery(event.target.value)}
+                  />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                {this.state.books && this.state.books.map(book => (
+                <li key={book.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+
+
+                      <div className="book-shelf-changer">
+                          <select key={book.name} value={book.shelf} onChange={(event) => this.updateShelf(book, event.target.value)}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                      </div>
+
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                   {/*<div className="book-authors">{book.authors}</div>
+                   */}
+                    <div className="book-authors">
+                      {book.authors.map(author => (
+                        <p key={author}> {author} </p>))
+                      }
+                    </div>
+                  </div>
+                </li>
+              ))
+            }
+              </ol>
             </div>
+
+
           </div>
         ) : (
-//console.log(this.state.books)
+
         //  <Route exact path='/' render={() => (
+
+        <div className="search-books-results">
+
            <ListBooks
               onChangeShelf={this.updateShelf}
               books={this.state.books}
            />
-        // )}/>
+
+           <div className="open-search">
+
+              <a onClick={() =>  this.setState({ showSearchPage: true })}>Add a book</a>
+
+          </div>
+
+        </div>
+
         )
+
+
     }
+
+
+
     </div>
+
     )
   }
 }
